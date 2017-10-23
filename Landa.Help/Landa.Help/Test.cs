d2ffaafc4e44dea6f503b8e16c2db61831e1c838
gaoconggit <718497701@qq.com>
2017-10-23 17:12:46 +0800

    init

---
+++ Landa.Help/Landa.Help/Test.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Landa.Help
+{
+   public class Test
+    {
+        public string Print()
+        {
+            return "hello,here is Landa !";
+        }
+    }
+}
